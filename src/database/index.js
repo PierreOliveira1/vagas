3e20a78af6a0816cf921cdc1028423a47f858972
Pierre Oliveira <pierre.torres445@gmail.com>
2023-06-11 11:55:40 -0300

    feat: muda diretório

---
+++ src/database/index.js
@@ -0,0 +1,13 @@
+const fakeData = [
+	{
+		id: 1,
+		username: 'joao.oliveira',
+		name: 'João Oliveira',
+		job: 'Desenvolvedor',
+		password: '$2a$10$UmmD9o76Kyzgfyg0gy2z7.yDUrvdCDLyy3PNcD.NCuN5281shNwfG',
+		permission: 'admin',
+		accessCount: 0,
+	},
+];
+
+module.exports = fakeData;
