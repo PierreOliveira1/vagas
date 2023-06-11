f531d0c700a8856b9642faf35d7900156963b383
Pierre Oliveira <pierre.torres445@gmail.com>
2023-06-11 12:01:34 -0300

    feat: cria um routes e configura todas as rotas

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+
+const teste1 = require('../controllers/users/teste1');
+const teste2 = require('../controllers/users/teste2');
+const teste3 = require('../controllers/users/teste3');
+const teste4 = require('../controllers/users/teste4');
+const teste5 = require('../controllers/users/teste5');
+const { auth } = require('../controllers/auth');
+const { checkToken } = require('../middlewares/checkToken');
+
+const routes = Router();
+
+routes.get('/user', teste1.getUser);
+routes.get('/users', teste1.getUsers);
+routes.post('/users', teste2);
+routes.delete('/users', checkToken, teste3);
+routes.patch('/users', checkToken, teste4);
+routes.get('/users/access', teste5);
+routes.post('/auth', auth);
+
+module.exports = routes;
